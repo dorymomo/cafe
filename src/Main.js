fdc1a3f8d54f5a399206cc4bd833eaacfc81d146
chang pyo <ckdvy0629@gmail.com>
2022-12-28 23:54:51 +0900

    main page commit

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,23 @@
+import "./Main.css";
+
 function Main() {
   return (
     <div clasName="App">
-        <h2>ggggg</h2>
+      <div className="ss">
+          <a href = ""><img src="./img/logo3.png" ></img></a> 
+      </div>
+      <div>
+        <br/><br/><br/><br/>
+        <div className="bbb">
+        <div><p className="mtext">카페 종류</p></div>
+        <form className="log">
+          <input type="id" placeholder=" 아이디 " />
+          <input type="password" placeholder="비밀번호" />
+          <button formAction="">로그인</button>
+          <button formAction="">회원가입</button>
+        </form>
+        </div>
+      </div>
     </div>
   );
 }
